213c695ab08a08f7c1c805b8306183d034ff6b4d
Zhos Dmitrii <zhos@bk.ru>
2024-10-14 13:27:09 +0500

    новый слайдер

---
+++ js/script.js
@@ -0,0 +1,11 @@
+new Swiper('.swiper',
+	{
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
+
+		slidesPerView: 5,
+		spaceBetween: 10,
+		freeMode: true,
+	});
